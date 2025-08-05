43ca5bab6bfedd6ff580d3ffb6de71880167a941
Meryem Yaren Küner <90837878+meryemyarenkunerr@users.noreply.github.com>
2025-08-05 15:19:02 +0300

    environment struct changed

--- includes/structs.h
+++ includes/structs.h
@@ -28,57 +28,33 @@ typedef struct s_token
 /* Command Structure */
 typedef struct s_command
 {
-	char				**args;
-	char				*input;
-	char				*out;
+	char				**arguments;
+	char				*input_file;
+	char				*output_file;
 	int					append_mode;
 	struct s_command	*next;
 }	t_command;
 
-/* Pipeline Structure */
-typedef struct s_redirect
+/* Pipeline Structure - Token Lists seperated by pipes */
+typedef struct s_pipeline
 {
 	t_token	**token_lists;
 	int		count;
-}	t_redirect;
+}	t_pipeline;
 
 /* Environment Structure */
 typedef struct s_env
 {
 	char			*key;
-	char			*content;
+	char			*value;
 	struct s_env	*next;
 }	t_env;
 
 /* Shell Structure */
 typedef struct s_shell
 {
-	// environment variables
-	char		**env;
-	t_env		*env_head;
-	char		**cmd_paths;
-	char		*user_name;					// ne kadar gerekli olduğuna bakacaazzz
-
-	// state flags
-	int			exit_code;
-	int			cmd_has_been_executed;		// TRUE or FALSE
-	int			print;						// TRUE or FALSE
-	int			color_codes;				// if color usage exists
-
-	// command and prompt info
-	char		*terminal_prompt;
-	t_command	*commands;					// command list
-
-	// heredoc
-	char		*heredoc_temp;
-	int			*expand_heredoc;
-
-	// input/output
-	int			std_fds[2];
-
-	// execution
-	int			exec_on_pipe;				// ne kadar gerekli olduğuna bakacaazzz (parser'dan gelen veriye bağlı)
-	int			execute;					// ne zaman çalışacak buna karar veriyor
+	t_env	*environment;
+	int		exit_status;
 }	t_shell;
 
 #endif
