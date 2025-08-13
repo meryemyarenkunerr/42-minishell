cbcea094bf2694c48590dae2e07e613d26450d9b
Meryem Yaren Küner <90837878+meryemyarenkunerr@users.noreply.github.com>
2025-08-13 08:12:19 +0300

    export error handler added

--- srcs/error/errors.c
+++ srcs/error/errors.c
@@ -1,2 +1,7 @@
 #include "../../includes/minishell.h"
 
+void	handle_export_error(t_shell *shell, const char *arg)
+{
+	printf("export `%s': not a valid identifier\n", arg);
+	shell->exit_status = 1;
+}
