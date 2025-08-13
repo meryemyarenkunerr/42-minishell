1bb0112400f5da65246964f325558960c5a63302
Meryem Yaren Küner <90837878+meryemyarenkunerr@users.noreply.github.com>
2025-08-13 08:17:20 +0300

    ft_strncmp changed to ft_strcmp

--- srcs/executer/cmd_controller.c
+++ srcs/executer/cmd_controller.c
@@ -3,20 +3,20 @@
 // kontrol fonksiyonları
 int	is_input_redirection(const char *arg)
 {
-	return (ft_strncmp(arg, REDIRECTION_IN, 1) == 0 && arg[1] == '\0');
+	return (ft_strcmp(arg, REDIRECTION_IN) == 0 && arg[1] == '\0');
 }
 
 int	is_output_redirection(const char *arg)
 {
-	return (ft_strncmp(arg, REDIRECTION_OUT, 1) == 0 && arg[1] == '\0');
+	return (ft_strcmp(arg, REDIRECTION_OUT) == 0 && arg[1] == '\0');
 }
 
 int	is_appended_redirection(const char *arg)
 {
-	return (ft_strncmp(arg, REDIRECTION_APPEND, 2) == 0);
+	return (ft_strcmp(arg, REDIRECTION_APPEND) == 0);
 }
 
 int	is_heredoc_redirection(const char *arg)
 {
-	return (ft_strncmp(arg, REDIRECTION_HEREDOC, 2) == 0);
+	return (ft_strcmp(arg, REDIRECTION_HEREDOC) == 0);
 }
