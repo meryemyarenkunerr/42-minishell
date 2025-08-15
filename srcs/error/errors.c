81d5c279e339a8767b65202008fec78a871fe669
Meryem Yaren Küner <90837878+meryemyarenkunerr@users.noreply.github.com>
2025-08-15 04:04:34 +0300

    fork error handler added

--- srcs/error/errors.c
+++ srcs/error/errors.c
@@ -5,3 +5,13 @@ void	handle_export_error(t_shell *shell, const char *arg)
 	printf("export `%s': not a valid identifier\n", arg);
 	shell->exit_status = 1;
 }
+
+int	fork_error_handler(int fds[2], t_shell *shell)
+{
+	perror("fork");
+	close(fds[0]);
+	close(fds[1]);
+	signal(SIGINT, handle_sigint); // restore
+	shell->exit_status = 1;
+	return FALSE;
+}
